242f71ab8098d0c5039c3da065313d6ed4b2cce0
ObrigadoDeus <th_sola@hotmail.com>
2023-11-18 12:47:43 +0100

    Made collapsible improvements

--- assets/js/colapsible.js
+++ assets/js/colapsible.js
@@ -4,7 +4,8 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
+    var content = this.nextElementSibling || this.parentElement.querySelector(".collapsible.content");
+    
     if (content.style.maxHeight){
       content.style.maxHeight = null;
     } else {
